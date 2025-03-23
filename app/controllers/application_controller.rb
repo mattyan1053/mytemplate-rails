e9eb85908a63550e0e166dcdf0f809e226683130
mattyan1053 <h.matsumaruka14161@gmail.com>
2025-03-23 23:22:59 +0900

    :bug: Fix locale reverting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :set_locale, :set_sidebar_items
+  before_action :load_locale, :set_locale, :set_sidebar_items
 
   private
 
+  def load_locale
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+    session[:locale] = I18n.locale
   end
 
   def set_sidebar_items
