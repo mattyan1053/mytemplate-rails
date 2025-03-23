189ba27e7c624f24975fd026007bdf64b1150c0c
mattyan1053 <h.matsumaruka14161@gmail.com>
2025-03-23 17:34:30 +0900

    :gear: Setup typescript,eslint,prettier

--- app/typescript/controllers/application.ts
+++ app/typescript/controllers/application.ts
@@ -1,9 +1,15 @@
 import { Application } from "@hotwired/stimulus"
 
+declare global {
+    interface Window {
+        Stimulus: Application
+    }
+}
+
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
