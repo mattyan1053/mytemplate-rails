f88a357453268fe3022b6f4817581a36ba96e179
mattyan1053 <h.matsumaruka14161@gmail.com>
2025-03-23 19:32:01 +0900

    :art: Add sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @items = [
+      { name: "Home", url: "/" },
+      { name: "About", url: "/about" },
+      { name: "Contact", url: "/contact" }
+    ]
   end
 end
